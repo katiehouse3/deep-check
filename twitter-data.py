e80062c3b37db21fd9939cb0cf4ee6407d04997c
katiehouse3 <katiehouse3@gmail.com>
2019-03-24 07:54:13 -0400

    python ml

--- twitter-data.py
+++ twitter-data.py
@@ -1,16 +1,32 @@
-# Get the full list of followers of a particular user
-list_of_followers=[]
+# Import the necessary package to process data in JSON format
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
-current_cursor = tweepy.Cursor(api.followers_ids, screen_name="cocoweixu", count=5000)
-current_followers = current_cursor.iterator.next()
-list_of_followers.extend(current_followers)
-next_cursor_id = current_cursor.iterator.next_cursor
+# Import the tweepy library
+import tweepy
+import pprint
 
-while(next_cursor_id!=0):
-	current_cursor = tweepy.Cursor(self.api.followers_ids, screen_name="cocoweixu", count=5000,cursor=next_cursor_id)
-	current_followers=current_cursor.iterator.next()
-	list_of_followers.extend(current_followers)
-	next_cursor_id = current_cursor.iterator.next_cursor
-# Get a particular user's timeline (up to 200 of his/her most recent tweets)
-status_cursor = tweepy.Cursor(api.user_timeline, screen_name="billybob", count=200,tweet_mode='extended')
+pp = pprint.PrettyPrinter(indent=4)
+
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = '2620473211-RWN2g8omy31oxu7xu0b03fxSQ2rH2HrTMcgjAJL'
+ACCESS_SECRET = 'dySWOs8voQTlN9uaLOJNuN32v7QdQwMfNcNDCXV8O6Ku9'
+CONSUMER_KEY = '7KOV5jhvAty3QtBJurrllMggy'
+CONSUMER_SECRET = 'oSTU2ViKAuUHPZxRZ1Ha6OGv6jU8eXk5VGcc7N4wx6nxuYyH1l'
+
+# Setup tweepy to authenticate with Twitter credentials:
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+# Create the api to connect to twitter with your creadentials
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
+
+status_cursor = tweepy.Cursor(api.user_timeline, screen_name="dishsrivastava", count=100)
 status_list = status_cursor.iterator.next()
+for i in range(len(status_list)):
+	print(status_list[i].text)
+	#print(status_list[0].text)
